824c1058c62fea5840d31257a034dafc6c510966
alvarofonteles@gmail.com <alvarofonteles@gmail.com>
2022-03-01 11:49:58 -0300

    Eclipse | Codegate01 | Aula 2 Concluída

--- aula2_1/src/main/java/com/codegate/aula2/Emprego.java
+++ aula2_1/src/main/java/com/codegate/aula2/Emprego.java
@@ -4,7 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
@@ -14,16 +14,16 @@ public class Emprego {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
-	@NotEmpty(message = "Titulo não pode ser nulo.")
-	@Size(min = 5, max = 20, message = "Mínimo de 05 Caracteres.")
+	@NotNull
+	@Size(min = 4)
 	private String titulo;
 
-	@NotEmpty(message = "Empresa não pode ser nula.")
-	@Size(min = 3, max = 50, message = "Mínimo de 03 Caracteres e Máximo de 50.")
+	@NotNull
+	@Size(min = 3)
 	private String empresa;
 
-	@NotEmpty(message = "Descrição não pode ser nula.")
-	@Size(min = 10, max = 100, message = "Mínimo de 10 Caracteres e Máximo de 100.")
+	@NotNull
+	@Size(min = 10)
 	private String descricao;
 
 	public long getId() {
---
+++ aula2_1/src/main/java/com/codegate/aula2/EmpregoRepository.java
@@ -0,0 +1,10 @@
+package com.codegate.aula2;
+
+import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+
+//@Repository
+public interface EmpregoRepository extends CrudRepository<Emprego, Long> {
+
+	
+}
