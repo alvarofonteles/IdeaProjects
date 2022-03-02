7e7c84166ed68c2ce8e72068c88b97ba179ae1b8
alvarofonteles@gmail.com <alvarofonteles@gmail.com>
2022-03-02 15:30:26 -0300

    Eclipse | Codegate01 | spring-security | Concluída BootStrap.

--- fragments/src/main/java/com/codegate01/fragments/FragmentApplication.java
+++ fragments/src/main/java/com/codegate01/fragments/FragmentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FragmentsApplication {
+public class FragmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FragmentsApplication.class, args);
+		SpringApplication.run(FragmentApplication.class, args);
 	}
 
 }
